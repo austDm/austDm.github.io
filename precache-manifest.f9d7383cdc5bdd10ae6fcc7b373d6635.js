6b9048e7c59d38ceaf5862fdd87331986e0173c2
Dmitry_Aust <Dmitry_Aust@epam.com>
2019-09-06 11:50:39 +0300

    Updates

---
+++ precache-manifest.f9d7383cdc5bdd10ae6fcc7b373d6635.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5fd68f1c79a9e7736eb220d92309b7a",
+    "url": "/games/index.html"
+  },
+  {
+    "revision": "4f7e50939f5ff31d364b",
+    "url": "/games/static/css/main.a12bbf41.chunk.css"
+  },
+  {
+    "revision": "2b29faf313d2bb50c3d4",
+    "url": "/games/static/js/2.b89de1e2.chunk.js"
+  },
+  {
+    "revision": "4f7e50939f5ff31d364b",
+    "url": "/games/static/js/main.079da30c.chunk.js"
+  },
+  {
+    "revision": "bd248bff26a526f6018b",
+    "url": "/games/static/js/runtime~main.ccf7e7e0.js"
+  },
+  {
+    "revision": "6f23a622bc4a404bf8f85266471359a3",
+    "url": "/games/static/media/2.6f23a622.jpg"
+  },
+  {
+    "revision": "f16616a95e83968bfcf71780c40c395f",
+    "url": "/games/static/media/Sacramento-Regular.f16616a9.ttf"
+  },
+  {
+    "revision": "475b539094d914ff68e420b4571c96c3",
+    "url": "/games/static/media/TextMeOne-Regular.475b5390.ttf"
+  },
+  {
+    "revision": "57c0838404ffc8791812b29baf5de59c",
+    "url": "/games/static/media/Zapped Sticks.57c08384.ttf"
+  },
+  {
+    "revision": "e30c932818080a9f02eb6a0d7bfbda8a",
+    "url": "/games/static/media/ava2.e30c9328.jpg"
+  },
+  {
+    "revision": "1045072d235038b3f62bc6226279c816",
+    "url": "/games/static/media/background.1045072d.jpg"
+  },
+  {
+    "revision": "a279a640837a75728fc28a295fbd8d69",
+    "url": "/games/static/media/bg.a279a640.jpg"
+  },
+  {
+    "revision": "9d1b6ae54dcbe068f3ba20c9821b4cff",
+    "url": "/games/static/media/budmo jigglish.9d1b6ae5.ttf"
+  },
+  {
+    "revision": "a43a0387c9c7cb5ce6c9aa3db937f065",
+    "url": "/games/static/media/coin1.a43a0387.png"
+  },
+  {
+    "revision": "d3aeda3294d818187345ad809b75e049",
+    "url": "/games/static/media/coin2.d3aeda32.png"
+  },
+  {
+    "revision": "b93f984f07732c68bf6b05b9ed73a96c",
+    "url": "/games/static/media/granny-2.b93f984f.png"
+  },
+  {
+    "revision": "a159af3f5a12afbfa89e24f94586b10b",
+    "url": "/games/static/media/sss.a159af3f.woff2"
+  }
+]);
