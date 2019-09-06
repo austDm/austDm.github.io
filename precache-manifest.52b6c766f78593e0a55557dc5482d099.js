fafd396727ab3066e39613236168635aa2ec5ffc
Dmitry_Aust <Dmitry_Aust@epam.com>
2019-09-06 11:52:47 +0300

    Updates

---
+++ precache-manifest.52b6c766f78593e0a55557dc5482d099.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b60cf5e780751e56507028d9d7b5b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d02cb8fd568d579a9cd",
+    "url": "/static/css/main.bccae92b.chunk.css"
+  },
+  {
+    "revision": "2b29faf313d2bb50c3d4",
+    "url": "/static/js/2.b89de1e2.chunk.js"
+  },
+  {
+    "revision": "4d02cb8fd568d579a9cd",
+    "url": "/static/js/main.079da30c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6f23a622bc4a404bf8f85266471359a3",
+    "url": "/static/media/2.6f23a622.jpg"
+  },
+  {
+    "revision": "f16616a95e83968bfcf71780c40c395f",
+    "url": "/static/media/Sacramento-Regular.f16616a9.ttf"
+  },
+  {
+    "revision": "475b539094d914ff68e420b4571c96c3",
+    "url": "/static/media/TextMeOne-Regular.475b5390.ttf"
+  },
+  {
+    "revision": "57c0838404ffc8791812b29baf5de59c",
+    "url": "/static/media/Zapped Sticks.57c08384.ttf"
+  },
+  {
+    "revision": "e30c932818080a9f02eb6a0d7bfbda8a",
+    "url": "/static/media/ava2.e30c9328.jpg"
+  },
+  {
+    "revision": "1045072d235038b3f62bc6226279c816",
+    "url": "/static/media/background.1045072d.jpg"
+  },
+  {
+    "revision": "a279a640837a75728fc28a295fbd8d69",
+    "url": "/static/media/bg.a279a640.jpg"
+  },
+  {
+    "revision": "9d1b6ae54dcbe068f3ba20c9821b4cff",
+    "url": "/static/media/budmo jigglish.9d1b6ae5.ttf"
+  },
+  {
+    "revision": "a43a0387c9c7cb5ce6c9aa3db937f065",
+    "url": "/static/media/coin1.a43a0387.png"
+  },
+  {
+    "revision": "d3aeda3294d818187345ad809b75e049",
+    "url": "/static/media/coin2.d3aeda32.png"
+  },
+  {
+    "revision": "b93f984f07732c68bf6b05b9ed73a96c",
+    "url": "/static/media/granny-2.b93f984f.png"
+  },
+  {
+    "revision": "a159af3f5a12afbfa89e24f94586b10b",
+    "url": "/static/media/sss.a159af3f.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/games/precache-manifest.f9d7383cdc5bdd10ae6fcc7b373d6635.js"
+  "/precache-manifest.52b6c766f78593e0a55557dc5482d099.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/games/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
